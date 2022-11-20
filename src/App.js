cc3769cac995896a472cf54bed9086571183de5e
prasanth13new <prasanth13new@gmail.com>
2022-11-21 01:04:18 +0530

    .env

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8&page=${page}`;
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_API_KEY}&page=${page}`
 
     const response = await fetch(url);
     const responseJson = await response.json();
